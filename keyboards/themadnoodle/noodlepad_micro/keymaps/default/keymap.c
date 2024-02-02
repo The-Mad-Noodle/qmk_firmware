929024d0870d3c43e178b12f1cd6dcace2e8ef77
Jesse Leventhal <45154268+jessel92@users.noreply.github.com>
2024-02-02 14:39:42 -0500

    Micro Defualt Done

--- keyboards/themadnoodle/noodlepad_micro/keymaps/default/keymap.c
+++ keyboards/themadnoodle/noodlepad_micro/keymaps/default/keymap.c
@@ -2,9 +2,15 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+
 enum custom_keycodes {
-    // existing keycodes
-    RGB_IND = SAFE_RANGE // add this line for the LED toggle
+#ifdef VIA_ENABLE    // If you are using VIA, you can use the same keycodes as the default keymap
+    L_IND = QK_KB_0, // Toggle the Layer Indicators Modes
+    L_CYC            // Cycle through the layers
+#else                // If you are not using VIA, you can use your own keycodes
+    L_IND = SAFE_RANGE,
+    L_CYC
+#endif
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -22,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
       KC_MPRV,           KC_MNXT, 
       KC_MSTP, KC_MPLY, KC_MSEL,
-      LT(2,RGB_IND), KC_MAIL, LT(1, KC_MYCM)
+      LT(2,KC_CALC), KC_MAIL, LT(1, KC_MYCM)
       ),
 
 
@@ -56,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = LAYOUT(
       KC_TRNS,          KC_TRNS, 
       KC_TRNS, KC_TRNS, KC_TRNS, 
-      KC_TRNS, KC_TRNS, KC_TRNS
+      KC_TRNS, L_IND, KC_TRNS
     )
 };
 
@@ -71,14 +77,17 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 };
 #endif
 
+//  ======================Layer Light Setup==========================
+
 const rgblight_segment_t PROGMEM layer_zero_all[]  = RGBLIGHT_LAYER_SEGMENTS({0, 4, HSV_WHITE});
 const rgblight_segment_t PROGMEM layer_one_all[]   = RGBLIGHT_LAYER_SEGMENTS({0, 4, HSV_RED});
 const rgblight_segment_t PROGMEM layer_two_all[]   = RGBLIGHT_LAYER_SEGMENTS({0, 4, HSV_GREEN});
 const rgblight_segment_t PROGMEM layer_three_all[] = RGBLIGHT_LAYER_SEGMENTS({0, 4, HSV_BLUE});
-const rgblight_segment_t PROGMEM layer_zero[]      = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_WHITE});
-const rgblight_segment_t PROGMEM layer_one[]       = RGBLIGHT_LAYER_SEGMENTS({1, 1, HSV_WHITE});
-const rgblight_segment_t PROGMEM layer_two[]       = RGBLIGHT_LAYER_SEGMENTS({2, 1, HSV_WHITE});
-const rgblight_segment_t PROGMEM layer_three[]     = RGBLIGHT_LAYER_SEGMENTS({3, 1, HSV_WHITE});
+
+const rgblight_segment_t PROGMEM layer_zero[]  = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_WHITE});
+const rgblight_segment_t PROGMEM layer_one[]   = RGBLIGHT_LAYER_SEGMENTS({1, 1, HSV_WHITE});
+const rgblight_segment_t PROGMEM layer_two[]   = RGBLIGHT_LAYER_SEGMENTS({2, 1, HSV_WHITE});
+const rgblight_segment_t PROGMEM layer_three[] = RGBLIGHT_LAYER_SEGMENTS({3, 1, HSV_WHITE});
 
 const rgblight_segment_t *const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
 
@@ -95,20 +104,83 @@ const rgblight_segment_t *const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
 
 void keyboard_post_init_user(void) {
     // Enable the LED layers
-
     rgblight_layers = my_rgb_layers;
 }
 
-bool led_mode = false; // false for first LED mode, true for second LED mode
+//  ======================Custom Keycodes==========================
+
+// 1st layer on the cycle
+#define LAYER_CYCLE_START 0
+// Last layer on the cycle
+#define LAYER_CYCLE_END 3
+
+bool led_mode; // false for Blinking Mode, true for Static mode
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case RGB_IND:
+        case L_IND:
             if (record->event.pressed) {
-                // Code to toggle between the two LED modes
+                // Code to toggle between the two Layer Indicators
                 led_mode = !led_mode; // Toggle the mode
+
+                // Updateing lighting layer when key is pressed
+                if (led_mode) {
+                    if (layer_state_is(0)) {
+                        rgblight_set_layer_state(4, true);
+                    }
+                    if (layer_state_is(1)) {
+                        rgblight_set_layer_state(5, true);
+                    }
+                    if (layer_state_is(2)) {
+                        rgblight_set_layer_state(6, true);
+                    }
+                    if (layer_state_is(3)) {
+                        rgblight_set_layer_state(7, true);
+                    }
+                } else {
+                    rgblight_set_layer_state(4, false);
+                    rgblight_set_layer_state(5, false);
+                    rgblight_set_layer_state(6, false);
+                    rgblight_set_layer_state(7, false);
+
+                    if (layer_state_is(0)) {
+                        rgblight_blink_layer(0, 1000);
+                    }
+                    if (layer_state_is(1)) {
+                        rgblight_blink_layer(1, 1000);
+                    }
+                    if (layer_state_is(2)) {
+                        rgblight_blink_layer(2, 1000);
+                    }
+                    if (layer_state_is(3)) {
+                        rgblight_blink_layer(3, 1000);
+                    }
+                }
+            } else {
             }
             return false; // Skip all further processing of this key
+
+        case L_CYC:
+            // Our logic will happen on presses, nothing is done on releases
+            if (!record->event.pressed) {
+                // We've already handled the keycode (doing nothing), let QMK know so no further code is run unnecessarily
+                return false;
+            }
+
+            uint8_t current_layer = get_highest_layer(layer_state);
+
+            // Check if we are within the range, if not quit
+            if (current_layer > LAYER_CYCLE_END || current_layer < LAYER_CYCLE_START) {
+                return false;
+            }
+
+            uint8_t next_layer = current_layer + 1;
+            if (next_layer > LAYER_CYCLE_END) {
+                next_layer = LAYER_CYCLE_START;
+            }
+            layer_move(next_layer);
+            return false;
+
         default:
             return true; // Process all other keycodes normally
     }
@@ -116,37 +188,35 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     if (led_mode) {
-        // Code from the second block for blinking lights
+        // Code for Static Layer Indicators
+
+        rgblight_set_layer_state(4, layer_state_cmp(state, 0));
+        rgblight_set_layer_state(5, layer_state_cmp(state, 1));
+        rgblight_set_layer_state(6, layer_state_cmp(state, 2));
+        rgblight_set_layer_state(7, layer_state_cmp(state, 3));
+
+    } else {
+        // Code for Blinking Layer Indicators
 
         uint8_t layer = get_highest_layer(state);
 
         switch (layer) {
             case 0:
-                rgblight_blink_layer(0, 1000);
+                rgblight_blink_layer(0, 500);
                 break;
             case 1:
-                rgblight_blink_layer(1, 1000);
+                rgblight_blink_layer(1, 500);
                 break;
             case 2:
-                rgblight_blink_layer(2, 1000);
+                rgblight_blink_layer(2, 500);
                 break;
             case 3:
-                rgblight_blink_layer(3, 1000);
+                rgblight_blink_layer(3, 500);
                 break;
 
             default:
-                rgblight_blink_layer(0, 1000);
+                rgblight_blink_layer(0, 500);
         }
-    } else {
-        // Code from the first block for static lights
-
-        rgblight_set_layer_state(4, layer_state_cmp(state, 0));
-
-        rgblight_set_layer_state(5, layer_state_cmp(state, 1));
-
-        rgblight_set_layer_state(6, layer_state_cmp(state, 2));
-
-        rgblight_set_layer_state(7, layer_state_cmp(state, 3));
     }
     return state;
 }
